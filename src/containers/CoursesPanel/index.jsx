c1d0dfa968082be7794e6d08d4e3ac8d0e370b58
emanOsama <emanosama@maids.cc>
2024-11-01 13:03:14 +0200

    test text

--- src/containers/CoursesPanel/index.jsx
+++ src/containers/CoursesPanel/index.jsx
@@ -27,7 +27,7 @@ export const CoursesPanel = () => {
   return (
     <div className="course-list-container">
       <div className="course-list-heading-container">
-        <h2 className="course-list-title">{formatMessage(messages.myCourses)}</h2>
+        <h2 className="course-list-title TEST-title-eo"> TEST-title-eo {formatMessage(messages.myCourses)}</h2>
         <div className="course-filter-controls-container">
           <CourseFilterControls {...courseListData.filterOptions} />
         </div>
